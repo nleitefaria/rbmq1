54e354244e56f72518abcecdd1f34287646a1f97
nleitefaria <nleitefaria@gmail.com>
2023-07-10 16:53:43 +0100

    INIT

---
+++ src/main/java/com/nleitefaria/rbmq1/Rbmq1Application.java
@@ -0,0 +1,13 @@
+package com.nleitefaria.rbmq1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rbmq1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rbmq1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nleitefaria/rbmq1/Rbmq1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nleitefaria.rbmq1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rbmq1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
